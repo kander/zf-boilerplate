e774356e9f66373f43e1159f81c8e8009b2c8dce
Sander Bol <sander.bol@clansman.nl>
2013-07-25 15:22:22 +0200

    Moved to Composer version of Elastica. ElasticSearch isn't working yet… trying to find out why.

--- data/cookbooks/elasticsearch/recipes/default.rb
+++ data/cookbooks/elasticsearch/recipes/default.rb
@@ -24,7 +24,7 @@ package "unzip" do
 end
 
 remote_file "/tmp/elasticsearch-#{node[:elasticsearch][:version]}.zip" do
-  source "http://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-#{node[:elasticsearch][:version]}.zip"
+  source "https://github.com/elasticsearch/elasticsearch/archive/v#{node[:elasticsearch][:version]}.zip"
   mode "0644"
   checksum node[:elasticsearch][:checksum]
 end
