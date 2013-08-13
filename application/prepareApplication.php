dc61f93c05c6a446d95316609b2c027d004a846c
Sander Bol <sander.bol@clansman.nl>
2013-08-13 09:08:59 +0200

    Moved bootstrapping code from the various application entry points into single file

---
+++ application/prepareApplication.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ Contains initialization code shared by the various entrypoints into the application.
+ Current entrypoints are:
+ - public/index.php
+ - public/ws/soap.php
+ - bin/bootstrap.php
+ - tests/bootstrap.php
+*/
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+|| define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
+
+// Ensure library/ is on include_path
+set_include_path(
+    implode(
+        PATH_SEPARATOR,
+        array(
+             realpath(APPLICATION_PATH . '/../library'),
+             get_include_path(),
+        )
+    )
+);
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+
+spl_autoload_unregister(array('Zend_Loader_Autoloader','autoload'));
+
+return $application;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,30 +1,3 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// Define path to application directory
-defined('APPLICATION_PATH')
-|| define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-
-// Define application environment
-defined('APPLICATION_ENV')
-|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
-
-// Ensure library/ is on include_path
-set_include_path(
-    implode(
-        PATH_SEPARATOR,
-        array(
-            realpath(APPLICATION_PATH . '/../library'),
-            get_include_path(),
-        )
-    )
-);
-
-// Create application, bootstrap, and run
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-
-spl_autoload_unregister(array('Zend_Loader_Autoloader','autoload'));
+$application = require_once __DIR__ . '/../application/prepareApplication.php';
 $application->bootstrap();
